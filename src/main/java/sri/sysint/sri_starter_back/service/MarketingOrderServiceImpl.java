669e7d35272ced1972913598fcb362a0fdffebd1
ralfadzila <153299863+ralfadzila@users.noreply.github.com>
2025-02-13 13:53:19 +0700

    Update MarketingOrderServiceImpl.java

--- src/main/java/sri/sysint/sri_starter_back/service/MarketingOrderServiceImpl.java
+++ src/main/java/sri/sysint/sri_starter_back/service/MarketingOrderServiceImpl.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -1451,7 +1452,7 @@ public class MarketingOrderServiceImpl {
     }
     
     //Save Ar Defect Reject
-    public int saveArDefectReject(SaveFinalMarketingOrder mo) {
+    public int saveArDefectReject(SaveFinalMarketingOrder mo) throws ParseException {
     	String moIdFed = "";
     	moIdFed = getLastIdMo();
     	
@@ -1617,7 +1618,14 @@ public class MarketingOrderServiceImpl {
         	dataMarketingFed.setRevisionPpc(dataMarketingFed.getRevisionPpc().add(BigDecimal.ONE)); // Tambah 1 pada revisi
         }
 
-            BigDecimal TopVAfterArRjDf = marketingOrderRepo.findTopVAfterArRjDf(dataMarketingFed.getType(), dataMarketingFed.getMonth0().toString());
+        String dateStr = dataMarketingFed.getMonth0().toString();
+        SimpleDateFormat originalFormat = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy");
+        SimpleDateFormat targetFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        Date date = originalFormat.parse(dateStr);
+        String formatedtanggal = targetFormat.format(date);
+        
+            BigDecimal TopVAfterArRjDf = marketingOrderRepo.findTopVAfterArRjDf(dataMarketingFed.getType(), formatedtanggal);
             BigDecimal NewVAfterArRjDf = TopVAfterArRjDf.add(BigDecimal.ONE);
 
             dataMarketingFed.setvBeforeArRjDf(dataMarketingFed.getvBeforeArRjDf());
